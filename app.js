c01f932dcd563fc9574e82d3af379db4be8f6c26
Emnon <csampaie.5@gmail.com>
2021-05-07 21:55:14 +0200

    FINAL

---
+++ app.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: 'fade',
+  speed: 5000,
+  fadeEffect: {
+    crossFade: true
+  },
+  autoplay: {
+    delay: 3000,
+  },
+
+});
